dbb49cd88108a21eef2beee5d43d22d471a1f150
Endy Apinageri <apinaendy@gmail.com>
2020-06-01 14:59:09 +0100

    Fix date format, converted to time to string

--- models/task-tracker.go
+++ models/task-tracker.go
@@ -17,9 +17,9 @@ func CreateTaskTrack(user User, task TaskTracker) interface{} {
 		return ValidResponse(403, err.Error(), "error")
 	}
 
-	layout := "2006-01-02 15:04:05"
-	str := "0000-00-00 00:00:01"
-	t, _ := time.Parse(layout, str)
+	// layout := "2006-01-02 15:04:05"
+	// str := "0000-00-00 00:00:01"
+	// t, _ := time.Parse(layout, str)
 
 	task.StartTime = time.Time{}
 	task.EndTime = time.Time{}
